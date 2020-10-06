b3b0a8359aa5f94de6f624d1883da725e0f45a53
Gokhan Namal <gokhanamal@users.noreply.github.com>
2020-10-06 15:31:25 +0300

    Update NetworkManager
    I made some code quality improvement to improve code readability. You don't have create a Result type, it is aldready defined after swift 4 :)

--- My Ptown/WebService/NetworkManager.swift
+++ My Ptown/WebService/NetworkManager.swift
@@ -10,21 +10,19 @@ import Foundation
 
 struct NetworkManager {
     
-    func performRequest<T: Decodable>(url: String, completion: @escaping (NetworkResponse<T, NetworkError>) -> Void) {
-        if let url = URL(string: url) {
-            URLSession.shared.dataTask(with: url) { (data, response, error) in
-                if error != nil {
-                    completion(NetworkResponse.failure(.network))
-                } else {
-                    if let safeData = data {
-                        if let decodedData = try? JSONDecoder().decode(T.self, from: safeData) {
-                            completion(NetworkResponse.success(decodedData))
-                        } else {
-                            completion(NetworkResponse.failure(.decoding))
-                        }
-                    }
-                }
-            }.resume()
+    func performRequest<T: Decodable>(url: String, completion: @escaping (Result<T, NetworkError>) -> Void) {
+        guard let url = URL(string: url) else { return }
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else {
+             completion(NetworkResponse.failure(.network))
+            }
+            do {
+                let response = try JSONDecoder().decode(T.self, from: data)
+                completion(.success(response))
+            } catch {
+                completion(.failure(.decoding))
+            }
         }
+        task.resume()
     }
 }
